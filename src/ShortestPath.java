528158892bc147c15b3f98ba39ca848315d9fc6c
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-25 22:27:00 +0530

    str

---
+++ src/ShortestPath.java
@@ -0,0 +1,28 @@
+public class ShortestPath {
+
+    public static float shortestPath(String path){
+
+        int x = 0 , y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            if(path.charAt(i) == 'N'){
+                y++;
+            }
+            if(path.charAt(i) == 'S'){
+                y--;
+            }
+            if(path.charAt(i) == 'E'){
+                x++;
+            } else {
+                x--;
+            }
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+        return (float) Math.sqrt(X2 + Y2);
+    }
+
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(shortestPath(path));
+    }
+}
