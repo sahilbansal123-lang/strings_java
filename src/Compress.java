32f757e259290fb63c788908027f461497f79f56
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-05-26 09:35:20 +0530

    str

---
+++ src/Compress.java
@@ -0,0 +1,23 @@
+public class Compress {
+
+    public static String compress(String str) {
+        StringBuilder bs = new StringBuilder("");
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            bs.append(str.charAt(i));
+            if (count > 1) {
+                bs.append(count.toString());
+            }
+        }
+        return bs.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabbcccccdd";
+        System.out.println(compress(str));
+    }
+}
