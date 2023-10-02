c19fa0814f7f0ce0d7945200ab8e565011eb18e1
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-02 11:30:13 +0530

    str

--- src/RemoveConsecutiveCharacter.java
+++ src/RemoveConsecutiveCharacter.java
@@ -1,7 +1,26 @@
 public class RemoveConsecutiveCharacter {
 
+    public static String remove(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        StringBuilder result = new StringBuilder();
+        char currChar = str.charAt(0);
+        result.append(currChar);
+
+        for (int i = 1; i < str.length(); i++) {
+            char nextChar = str.charAt(i);
+            if (currChar != nextChar) {
+                result.append(nextChar);
+                currChar = nextChar;
+            }
+        }
+        return result.toString();
+    }
+
 
     public static void main(String[] args) {
-
+        System.out.println(remove("aaabba"));
     }
 }
