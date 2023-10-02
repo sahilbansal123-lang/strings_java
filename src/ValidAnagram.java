9c6bfa6a5a56d4e3aa38c8e63b48ed1d805962d8
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-02 10:33:21 +0530

    str

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -1,2 +1,28 @@
 public class ValidAnagram {
+
+    public static boolean validAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int [] charCount = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            charCount[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < charCount.length; i++) {
+            if (charCount[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validAnagram("sahil", "lias"));
+    }
 }
