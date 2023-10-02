2bf32d2ceb2b848a0e298088cd1e7211e20e731d
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-02 11:57:51 +0530

    str

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,2 +1,27 @@
 public class LongestCommonPrefix {
+
+    public static String prefix(String[] strs) {
+        String shortest = strs[0];
+
+        // Shortest Length String
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length()<shortest.length()){
+                shortest = strs[i];
+            }
+        }
+
+        for (int i = 0; i < strs.length; i++) {
+            char c = shortest.charAt(i);
+            for (String str : strs) {
+                if (c != str.charAt(i)) {
+                    return str.substring(0, i);
+                }
+            }
+        }
+        return shortest;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
