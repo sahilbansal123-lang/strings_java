97186729a144749bfb7b797830775fb9c07499e9
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-03 10:49:19 +0530

    str

--- src/PrintCountOfDuplicates.java
+++ src/PrintCountOfDuplicates.java
@@ -1,12 +1,28 @@
+import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class PrintCountOfDuplicates {
 
-    public static void duplicacy(String s) {
-        Map
+    public static void duplicacy(String s) {       // Using MAP
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + " , " + entry.getValue());
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+        String s = "geeksforgeeks";
+        duplicacy(s);
     }
 }
