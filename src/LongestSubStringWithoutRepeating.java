ed5c1e6ce9a1eb94a64c81d5ceef0ca64d0bef10
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-03 12:12:27 +0530

    str

--- src/LongestSubStringWithoutRepeating.java
+++ src/LongestSubStringWithoutRepeating.java
@@ -1,2 +1,36 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class LongestSubStringWithoutRepeating {
+
+    public static int substring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int maxLength = 0;
+        int left = 0; // Left pointer of the sliding window
+        Set<Character> charSet = new HashSet<>();
+
+        for (int right = 0; right < s.length(); right++) {
+            char currentChar = s.charAt(right);
+
+            while (charSet.contains(currentChar)) {
+                charSet.remove(s.charAt(left));
+                left++; // Move the left pointer to the right to remove the repeating character
+            }
+
+            charSet.add(currentChar); // Add the current character to the set
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcdaa";
+        System.out.println(substring(s));
+    }
 }
