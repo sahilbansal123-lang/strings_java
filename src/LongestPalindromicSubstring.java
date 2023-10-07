b36ca9d1c380b18ef7de32b911f384b660eaaf66
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-07 11:32:42 +0530

    str

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -1,7 +1,32 @@
 public class LongestPalindromicSubstring {
 
+    public static String longestPalindrome(String str) {
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            int len1 = expandFromCenter(str, i, i+1);
+            int len2 = expandFromCenter(str, i, i);
+            int len = Math.max(len1, len2);
+
+            if (end - start < len) {
+                start = i - (len - 1) / 2;
+                end = i + (len) / 2;
+            }
+        }
+        return str.substring(start, end);
+    }
+
+    public static int expandFromCenter(String str, int i, int j) {
+        while (i >= 0 && j < str.length() && str.charAt(i) == str.charAt(j)) {
+            i--;
+            j++;
+        }
+        return j - 1 - 1;
+    }
 
     public static void main(String[] args) {
-
+        System.out.println(longestPalindrome("abba"));
     }
 }
