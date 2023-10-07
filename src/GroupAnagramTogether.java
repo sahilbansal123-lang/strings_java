42ceb84546ef82671d140032c90d10db9dd335c9
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-07 10:02:53 +0530

    str

--- src/GroupAnagramTogether.java
+++ src/GroupAnagramTogether.java
@@ -1,7 +1,23 @@
+import java.util.*;
 public class GroupAnagramTogether {
 
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
 
+        for (String s : strs) {
+            char charArray[] = s.toCharArray();
+            Arrays.sort(charArray);
+            String sorted = new String(charArray);
+
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new LinkedList<String>());
+            }
+            map.get(sorted).add(s);
+        }
+        return new LinkedList<>(map.values());
+    }
     public static void main(String[] args) {
-
+        String[] strs = {"eat", "tea", "tan", "bat", "nat", "ate"};
+        System.out.println(groupAnagrams(strs));
     }
 }
